fbbfedee691a736fe624c9bc2847a66a5f69e17b
HugoLambrinidis <hlambrinidis@gmail.com>
2016-11-15 18:35:19 +0100

    Initial Commit

---
+++ SupWeather_HugoLambrinidis_Swift3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SupWeather_HugoLambrinidis_Swift3
+//
+//  Created by Hugo Lambrinidis on 15/11/16.
+//  Copyright © 2016 Hugo Lambrinidis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
